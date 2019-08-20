cd8730ea1c8860622df42e1038d840c6b8e830db
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-08-20 17:45:19 -0500

    finished closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,14 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+const up = 'dog';
+function returnUp () {
+  return up;
+}
+returnUp();
+
+console.log(returnUp())
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
